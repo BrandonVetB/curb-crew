083729f8681142e48b9db8cf9983585bb2b535d6
BrandonVetB <stelling.brandon@gmail.com>
2026-06-22 23:11:25 -0500

    Double header logo size, grow nav height to fit

--- main.js
+++ main.js
@@ -53,7 +53,7 @@
       e.preventDefault();
       nav.classList.remove("is-open");
       if (toggle) toggle.setAttribute("aria-expanded", "false");
-      var top = target.getBoundingClientRect().top + window.scrollY - 64;
+      var top = target.getBoundingClientRect().top + window.scrollY - 116;
       window.scrollTo({ top: top, behavior: REDUCED ? "auto" : "smooth" });
     });
   });
